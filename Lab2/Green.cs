1187ba64555a64a450d1972ca0efaee8cf7bd048
Буяндэлгэр Сэргэлэн <sera.sb1012@gmail.com>
2025-10-03 19:17:55 +0300

    Update Green.cs

--- Lab2/Green.cs
+++ Lab2/Green.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 
 namespace Lab2
 {
@@ -11,7 +11,11 @@ namespace Lab2
             double answer = 0;
 
             // code here
+            for (int i = 2; i <= n; i+= 2)
+            {
+                answer+=((double)i/(i+1.0));
 
+            }
             // end
 
             return answer;
@@ -21,7 +25,15 @@ namespace Lab2
             double answer = 0;
 
             // code here
-
+            if (x == 0)
+                return 0; 
+            
+            double currentTerm = 1.0; 
+            for (int i = 0; i <= n; i++)
+            {
+                answer += currentTerm;
+                currentTerm /= x; 
+            }
             // end
 
             return answer;
@@ -31,7 +43,13 @@ namespace Lab2
             long answer = 0;
 
             // code here
-
+            long factorial = 1;
+            for (int i = 0; i <= n; i++)
+            {
+                if (i > 0)
+                    factorial *= i;
+                answer += factorial;
+            }
             // end
 
             return answer;
@@ -41,7 +59,17 @@ namespace Lab2
             double answer = 0;
 
             // code here
-
+            int k = 1;
+            double term;
+            
+            do
+            {
+                double p = Math.Pow(x, k);
+                term = Math.Sin(k * p);
+                answer += term;
+                k++;
+            }
+            while (Math.Abs(term) >= E && k < 10000);  
             // end
 
             return answer;
@@ -51,7 +79,23 @@ namespace Lab2
             int answer = 0;
 
             // code here
-
+            if (Math.Abs(x) <= 1)
+                return 0;
+                
+            int n = 1;
+            double p = 1.0 / x;
+            double difference;
+            
+            do
+            {
+                n++;
+                double b = 1.0 /Math.Pow(x, n);
+                difference = Math.Abs(b - p);
+                p = b;
+            }
+            while (difference >= E && n < 10000);
+            
+            answer = n;
             // end
 
             return answer;
@@ -61,7 +105,16 @@ namespace Lab2
             int answer = 0;
 
             // code here
-
+            
+            int elem = 1;
+            int i = 0;
+            
+            while (elem < limit)
+            {
+                elem *= 2;
+                answer += elem;
+                i++;
+            }
             // end
 
             return answer;
@@ -72,7 +125,14 @@ namespace Lab2
             int answer = 0;
 
             // code here
-
+            double i  = L;
+            
+            while (i > Da)
+            {
+                i /= 2;
+                answer++;
+            }
+            
             // end
 
             return answer;
@@ -83,7 +143,22 @@ namespace Lab2
             double SY = 0;
 
             // code here
+            for (double x = a; x <= b + 0.0001; x += h)
+            {
+                int i = 0;
+                double temp;
+                double sum = 0;
 
+                do
+                {
+                    temp = Math.Pow(-1, i) * (Math.Pow(x, 2 * i + 1) / (2 * i + 1));
+                    sum += temp;
+                    i++;
+                } while (Math.Abs(temp) > E);
+    
+                SS += sum;
+                SY += Math.Atan(x);
+            }
             // end
 
             return (SS, SY);
